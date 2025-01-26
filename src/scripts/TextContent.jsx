13db0149647c0bc6cf98df028d0b530a5e660dcf
str <74649190+ugrijaniStr@users.noreply.github.com>
2025-01-26 21:56:15 +0100

    Create TextContent.jsx

---
+++ src/scripts/TextContent.jsx
@@ -0,0 +1,10 @@
+function TextContent() {
+    return (
+        <>
+            <h1>Challenges Informations</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit officia cumque inventore reprehenderit ipsa hic ea dolor impedit nostrum amet totam mollitia commodi.</p>
+        </>
+    );
+}
+
+export default TextContent;
